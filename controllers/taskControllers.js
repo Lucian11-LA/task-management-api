8dbfc45157b824e2631ae8b435418f95e1216326
Luciano Alfredo <lucianalfred60@gmail.com>
2024-12-24 20:35:58 +0100

    delete and update routes

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize');
 const { Task } = require('../models');
 
 const createTask = async (req, res) =>{
@@ -34,4 +35,65 @@ const getTasks = async (req, res)=>{
     }
 };
 
-module.exports = {createTask, getTasks};
+const deleteTask  = async (req, res)=>{
+    const  {idTask}  = req.parmas.id;
+
+    if(!idTask){
+        return res.status(400).json({message:'O ID e obrigatorio'});
+    }
+
+    try{
+
+        const task = await Task.findOne({
+            where:{
+                id: idTask,
+                userId: req.user.id
+            }
+        });
+
+        if(!task){
+            return res.status(404).json({message:"Tarefa nao encontrada"});
+        }
+
+        await task.destroy();
+
+        return res.status(200).json({message:"Tarefa excluida com sucess"});
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:'erro ao excluir a tarefa'});
+    }
+
+
+
+}
+
+const updateTask = async (req, res)=>{
+    const {Task} = require('../models');
+
+    const {id} = req.params;
+    const {title, description} = req.body;
+
+    if(!title){
+        return res.status(400).json({message:'o titulo e obrigatorio'});
+    }
+
+    try{
+        const task = await Task.findOne({where:{id, userId: req.user.id}});
+
+        if(!task){
+            return res.status(404).json({message:'tarefa nao encontrada!'});
+        }
+
+        await task.update({
+            title: title,
+            description: description
+        });
+
+        res.status(200).json(task);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:'erro ao editar a tarefa'});
+    }
+}
+module.exports = {createTask, getTasks, deleteTask, updateTask };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const authenticateJWT = require('../middleware/authenticate');
-const {createTask ,getTasks } = require('../controllers/taskControllers');
+const {createTask ,getTasks, deleteTask } = require('../controllers/taskControllers');
 
 const router = express.Router();
 
 router.post('/', authenticateJWT, createTask);
 router.get('/', authenticateJWT, getTasks);
-
+router.put('/:id',authenticateJWT,updateTask)
+router.delete('/:id',authenticateJWT,deleteTask)
 module.exports = router;
